05774c53a54ce3c9d7561f1e73ed1d507fe9543e
yourgotocoder <yourgotocoder@gmail.com>
2022-07-24 21:59:39 +0530

    Missing data compiled

--- resources/missingData.js
+++ resources/missingData.js
@@ -1,22 +1,22 @@
-const reader = require("simple-excel-to-json");
 const { MongoClient } = require("mongodb");
 const fs = require("fs");
 
-const data = reader.parseXls2Json(__dirname + "/4thAllotment.xlsx");
+const createMissingDataFile = (array, sem, field) => {
+    const filteredData = array.filter(student => student.current_sem === sem && !student[field]).map(student => student.regNo + " " + student.name);
+    fs.writeFileSync(`Missing${sem}thSem${field}.json`, JSON.stringify(filteredData));
+}
 // console.log(data[0]);
 const upload = async () => {
     const client = await MongoClient.connect(
-        process.env.MONGO_CONNECTION_STRING
+      "mongodb+srv://primary:SMIT01CSE@cluster0.i5fq9.mongodb.net/?retryWrites=true&w=majority"
     );
     const db = client.db("cse");
     const collection = db.collection("student-data");
     const result = await collection.find().toArray();
-    const filteredResult = result.filter(
-        (student) => student.current_sem === 5 && !student.elective_1
-    );
-    for (let i = 0; i < filteredResult.length; i++) {
-        fs.appendFileSync("Missing4thSem.txt", `${filteredResult[i].regNo} \n`);
-    }
+    createMissingDataFile(result, 5, 'cgpa');
+    createMissingDataFile(result, 7, 'cgpa');
+    createMissingDataFile(result, 5, 'emailId');
+    createMissingDataFile(result, 7, 'emailId');
 };
 
 upload()
