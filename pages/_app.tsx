7de353c794411e85ea82356555617b6ad511f49d
yourgotocoder <yourgotocoder@gmail.com>
2022-07-25 22:04:30 +0530

    Final change for a perfect score

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Head, Html } from "next/document";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Html lang="en">
+      <Component {...pageProps} />
+
+    </Html>
+  );
 }
 
 export default MyApp;
