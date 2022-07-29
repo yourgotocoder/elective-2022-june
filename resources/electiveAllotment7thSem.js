facf0aa3bab0edea965067acfee6786cd8a7e922
yourgotocoder <yourgotocoder@gmail.com>
2022-07-29 16:35:48 +0530

    Optimized seats done

--- resources/electiveAllotment7thSem.js
+++ resources/electiveAllotment7thSem.js
@@ -44,5 +44,148 @@ electiveAllotment()
             `data/PreAllotmentTransformedData7thSem.json`,
             JSON.stringify(data, null, 2)
         );
+
+        const selectionCountsPerSubject = data.reduce(
+            (previousValue, currentValue) => {
+                const keyElective1 = Object.keys(
+                    currentValue.elective_selections.first
+                );
+                for (let key of keyElective1) {
+                    if (
+                        !previousValue.elective_1[
+                            currentValue.elective_selections.first[key]
+                        ] &&
+                        currentValue.elective_selections.first[key]
+                    ) {
+                        previousValue.elective_1[
+                            currentValue.elective_selections.first[key]
+                        ] = 1;
+                    } else if (
+                        previousValue.elective_1[
+                            currentValue.elective_selections.first[key]
+                        ] &&
+                        currentValue.elective_selections.first[key] !== ""
+                    ) {
+                        previousValue.elective_1[
+                            currentValue.elective_selections.first[key]
+                        ] += 1;
+                    }
+                }
+
+                const keyElective2 = Object.keys(
+                    currentValue.elective_selections.second
+                );
+                for (let key of keyElective2) {
+                    if (
+                        !previousValue.elective_2[
+                            currentValue.elective_selections.second[key]
+                        ] &&
+                        currentValue.elective_selections.second[key]
+                    ) {
+                        previousValue.elective_2[
+                            currentValue.elective_selections.second[key]
+                        ] = 1;
+                    } else if (
+                        previousValue.elective_2[
+                            currentValue.elective_selections.second[key]
+                        ] &&
+                        currentValue.elective_selections.second[key] !== ""
+                    ) {
+                        previousValue.elective_2[
+                            currentValue.elective_selections.second[key]
+                        ] += 1;
+                    }
+                }
+
+                const keyElective3 = Object.keys(
+                    currentValue.elective_selections.third
+                );
+                for (let key of keyElective3) {
+                    if (
+                        !previousValue.elective_3[
+                            currentValue.elective_selections.third[key]
+                        ] &&
+                        currentValue.elective_selections.third[key]
+                    ) {
+                        previousValue.elective_3[
+                            currentValue.elective_selections.third[key]
+                        ] = 1;
+                    } else if (
+                        previousValue.elective_3[
+                            currentValue.elective_selections.third[key]
+                        ] &&
+                        currentValue.elective_selections.third[key] !== ""
+                    ) {
+                        previousValue.elective_3[
+                            currentValue.elective_selections.third[key]
+                        ] += 1;
+                    }
+                }
+                return previousValue;
+            },
+            { elective_1: {}, elective_2: {}, elective_3: {} }
+        );
+
+        const keysOfSelectionData = Object.keys(selectionCountsPerSubject);
+        const subjectNameElctive1 = Object.keys(
+            selectionCountsPerSubject.elective_1
+        );
+        const subjectNameElctive2 = Object.keys(
+            selectionCountsPerSubject.elective_2
+        );
+        const subjectNameElctive3 = Object.keys(
+            selectionCountsPerSubject.elective_3
+        );
+
+        let elective_1_total_selections = 0;
+        for (let key in selectionCountsPerSubject.elective_1) {
+            elective_1_total_selections +=
+                selectionCountsPerSubject.elective_1[key];
+        }
+
+        let elective_2_total_selections = 0;
+        for (let key in selectionCountsPerSubject.elective_2) {
+            elective_2_total_selections +=
+                selectionCountsPerSubject.elective_2[key];
+        }
+
+        let elective_3_total_selections = 0;
+        for (let key in selectionCountsPerSubject.elective_3) {
+            elective_3_total_selections +=
+                selectionCountsPerSubject.elective_3[key];
+        }
+
+        const optimizedSeatsElective1 = {};
+        for (let key in selectionCountsPerSubject.elective_1) {
+            optimizedSeatsElective1[key] = Math.ceil(
+                (selectionCountsPerSubject.elective_1[key] /
+                    elective_1_total_selections) *
+                    data.length
+            );
+        }
+
+        const optimizedSeatsElective2 = {};
+        for (let key in selectionCountsPerSubject.elective_2) {
+            optimizedSeatsElective2[key] = Math.ceil(
+                (selectionCountsPerSubject.elective_2[key] /
+                    elective_2_total_selections) *
+                    data.length
+            );
+        }
+
+        const optimizedSeatsElective3 = {};
+        for (let key in selectionCountsPerSubject.elective_3) {
+            optimizedSeatsElective3[key] = Math.ceil(
+                (selectionCountsPerSubject.elective_3[key] /
+                    elective_3_total_selections) *
+                    data.length
+            );
+        }
+        console.log(
+            optimizedSeatsElective1,
+            optimizedSeatsElective2,
+            optimizedSeatsElective3,
+            data.length
+        );
     })
     .catch((err) => console.log(err));
