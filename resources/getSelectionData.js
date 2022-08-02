c698e300640ad8138f8c63401ce6d0b21a238f7a
yourgotocoder <yourgotocoder@gmail.com>
2022-08-02 09:17:42 +0530

    For seeing selection data

---
+++ resources/getSelectionData.js
@@ -0,0 +1,74 @@
+const { MongoClient } = require("mongodb");
+const json2xls = require("json2xls");
+const fs = require("fs");
+
+const selectionData = async () => {
+    const client = await MongoClient.connect(
+        "mongodb+srv://primary:SMIT01CSE@cluster0.i5fq9.mongodb.net/?retryWrites=true&w=majority"
+    );
+
+    const db = client.db("cse");
+    const collection = db.collection("student-data");
+
+    const data = await collection.find().toArray();
+
+    await client.close();
+
+    const _7thSemData = data
+        .filter(
+            (student) =>
+                student.current_sem === 7 &&
+                student.cgpa &&
+                student.elective_selections
+        )
+        .sort((studentA, studentB) => studentB.cgpa - studentA.cgpa)
+        .map((student) => ({
+            REGNO: student.regNo,
+            NAME: student.name,
+            CGPA: student.cgpa,
+            elective_option_1_1: student.elective_selections.first.option1_1,
+            elective_option_1_2: student.elective_selections.first.option1_2,
+            elective_option_1_3: student.elective_selections.first?.option1_3,
+            elective_option_1_4: student.elective_selections.first?.option1_4,
+            elective_option_2_1: student.elective_selections.second.option2_1,
+            elective_option_2_2: student.elective_selections.second.option2_2,
+            elective_option_2_3: student.elective_selections.second?.option2_3,
+            elective_option_3_1: student.elective_selections.third.option3_1,
+            elective_option_3_2: student.elective_selections.third.option3_2,
+            elective_option_3_3: student.elective_selections.third.option3_3,
+            elective_option_3_4: student.elective_selections.third.option3_4,
+            elective_option_3_5: student.elective_selections.third.option3_5,
+            elective_option_3_6: student.elective_selections.third?.option3_6,
+            elective_option_3_7: student.elective_selections.third?.option3_7,
+            elective_option_3_8: student.elective_selections.third?.option3_8,
+        }));
+
+    const _5thSemData = data
+        .filter(
+            (student) =>
+                student.current_sem === 5 &&
+                student.cgpa &&
+                student.elective_selections
+        )
+        .sort((studentA, studentB) => studentB.cgpa - studentA.cgpa)
+        .map((student) => ({
+            REGNO: student.regNo,
+            NAME: student.name,
+            CGPA: student.cgpa,
+            elective_option_1_1: student.elective_selections.first.option1_1,
+            elective_option_1_2: student.elective_selections.first.option1_2,
+            elective_option_1_3: student.elective_selections.first.option1_3,
+            elective_option_2_1: student.elective_selections.second.option2_1,
+            elective_option_2_2: student.elective_selections.second.option2_2,
+            elective_option_2_3: student.elective_selections.second.option2_3,
+            elective_option_2_4: student.elective_selections.second.option2_4,
+        }));
+
+    return {_5thSemData, _7thSemData};
+};
+
+electiveAllotment()
+    .then((data) => {
+        console.log(data)
+    })
+    .catch((err) => console.log(err));
